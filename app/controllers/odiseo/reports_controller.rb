80e8722e306abc12f95b3a1063d20bb7d11dc3bc
Luis E. Guardiola <lguardiola@gmail.com>
2011-06-07 15:02:05 -0300

    fix namespace problem

--- app/controllers/odiseo/reports_controller.rb
+++ app/controllers/odiseo/reports_controller.rb
@@ -1,39 +1,40 @@
-class Odiseo::ReportsController < ApplicationController
+module Odiseo
+  class ReportsController < ApplicationController
 
-  before_filter :find_account
+    before_filter :find_account
 
-  respond_to :html#, :xml, :json
+    respond_to :html#, :xml, :json
 
 
-  # GET /odiseo/reports/new
-  # GET /odiseo/reports/new.json
-  # GET /odiseo/reports/new.xml
-  def new
-    @report = Report.new(@account)
-    respond_to do |format|
-      format.html # .html.erb
-    end
-  end
-
-  # POST /odiseo/reports
-  # POST /odiseo/reports.json
-  # POST /odiseo/reports.xml
-  def create
-    @report = Report.new(@account, params[:odiseo_report])
-    @details = @report.details
-    flash.clear
-    flash[:notice] = t('flash.actions.index.notice') if @report.valid? && @details.empty?
-    respond_to do |format|
-      format.html do
-        render :new
+    # GET /odiseo/reports/new
+    # GET /odiseo/reports/new.json
+    # GET /odiseo/reports/new.xml
+    def new
+      @report = Report.new(@account)
+      respond_to do |format|
+        format.html # .html.erb
       end
     end
-  end
 
-  protected
+    # POST /odiseo/reports
+    # POST /odiseo/reports.json
+    # POST /odiseo/reports.xml
+    def create
+      @report = Report.new(@account, params[:odiseo_report])
+      @details = @report.details
+      flash.clear
+      flash[:notice] = t('flash.actions.index.notice') if @report.valid? && @details.empty?
+      respond_to do |format|
+        format.html do
+          render :new
+        end
+      end
+    end
 
-  def find_account
-    @account = current_company.accounts.find(params[:account_id])
+    protected
+
+    def find_account
+      @account = current_company.accounts.find(params[:account_id])
+    end
   end
 end
-
