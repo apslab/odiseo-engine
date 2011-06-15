0841d0330107910d352772dd2d22a9890ca358b6
Luis E. Guardiola <lguardiola@gmail.com>
2011-06-15 15:17:12 -0300

    change to use getters

--- app/models/odiseo/report.rb
+++ app/models/odiseo/report.rb
@@ -10,13 +10,13 @@ module Odiseo
     def initialize(context, options = {})
       @context = context rescue nil
       @exercise_id = options.fetch(:exercise_id, nil)
-      @exercise = Exercise.find(@exercise_id) rescue nil      
+      @exercise = Exercise.find(exercise_id) rescue nil      
       @since_date = options.fetch(:since_date, '').to_date
       @until_date = options.fetch(:until_date, '').to_date
     end
 
     def date_between_exercise_date?
-      unless @exercise.present? && @since_date.between?(*@exercise.period) && @until_date.between?(*@exercise.period)
+      unless exercise.present? && since_date.between?(*exercise.period) && until_date.between?(*exercise.period)
         errors.add(:base, 'La fecha debe estar incluida en el periodo del ejercicio elegido!') 
       end
     end
@@ -28,8 +28,8 @@ module Odiseo
         .joins(:details)
         .where(:details => {context.class.model_name.foreign_key.to_sym => @context.id})
 =end
-      @context.details.joins(:entry)
-        .where(:entries => {:date_on => @since_date..@until_date, :exercise_id => @exercise.id})
+      context.details.joins(:entry)
+        .where(:entries => {:date_on => since_date..until_date, :exercise_id => exercise.id})
     end
 
     def to_key
